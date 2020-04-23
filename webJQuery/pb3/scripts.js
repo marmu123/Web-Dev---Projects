5700e57625d734db68ecfd6be7634eb897db48d0
marmu123 <marmureanumarius@yahoo.com>
2020-04-23 21:47:04 +0300

    added new mode for memory game(images->numbers)

--- webJQuery/pb3/scripts.js
+++ webJQuery/pb3/scripts.js
@@ -1,27 +1,14 @@
 
-var solved=[];
+var solved=0;
 var lastFlipped=null;
 var isPause=false;
-
-
-
-
-
+var typeOfGame="";
+const imagesURLs=["html.jpg","css.jpg","js.jpg","rails.jpg","python.jpg","node.jpg","html.jpg","css.jpg","js.jpg","rails.jpg","python.jpg","node.jpg"];
+const numbersURLs=["1","2","3","4","5","6","1","2","3","4","5","6"];
+var solvedUrls=[];
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-
-
-
-
-var BoxOpened = "";
-var ImgOpened = "";
-var Counter = 0;
-var ImgFound = 0;
-
-var board = "#board";
-
-
 
 function shuffleCards(imagesURLs){
     const cardsURL=imagesURLs;
@@ -31,13 +18,7 @@ function shuffleCards(imagesURLs){
     }
     return cardsURL;
 }
-const imagesURLs=["html.jpg","css.jpg","js.jpg","rails.jpg","python.jpg","node.jpg","html.jpg","css.jpg","js.jpg","rails.jpg","python.jpg","node.jpg"];
 
-
-$(function() {
-
-    flipAll()
-});
 
 
 
@@ -49,21 +30,26 @@ function initGame(rows=3,cols=4,images=imagesURLs) {
     isPause=false;
     while(mainBoard.firstChild)//reset the board
         mainBoard.removeChild(mainBoard.firstChild);
-    const cards=[];
-    const shuffleURL=shuffleCards(imagesURLs);
-   // const shuffleURL=images;
+    const shuffleURL=shuffleCards(images);
     for(i=1;i<=rows;i++){
         mainBoard.append("<div></div>");
         for(j=1;j<=cols;j++){
-            $("#board div:last-child").append("<img src='"+"backimg.jpg"+"' alt='NONE'/>");
+            if(typeOfGame==="image")
+                $("#board div:last-child").append("<img src='"+"backimg.jpg"+"' alt='NONE'/>");
+            else
+                $("#board div:last-child").append("<strong>X</strong>");
 
         }
 
     }
-    $("#board div img").addClass("card");
-    $("#board div img").each(function (index) {
+    if(typeOfGame==="image")
+        idk="img";
+    else
+        idk="strong";
+    $("#board div "+idk).addClass("card");
+    $("#board div "+idk).each(function (index) {
         $(this).bind("click",function () {
-            flipCard($(this),shuffleURL[index]);
+            flipCard($(this),shuffleURL[index],typeOfGame);
         });
 
     });
@@ -71,46 +57,51 @@ function initGame(rows=3,cols=4,images=imagesURLs) {
     return shuffleURL
 }
 
-function flipAll() {
-    var urls=initGame();
-    isPause=true
+function flipAll(initdata) {
+    var urls=initGame(3,4,initdata);
+    var idk="";
+    if(typeOfGame==="image")
+        idk="img";
+    else
+        idk="strong";
+    isPause=true;
     for(var i =0;i<urls.length;i++){
-        var el=$("#board div img").eq(i);
-        el.addClass("flip");
-        el.attr("src",urls[i]);
+        var el=$("#board div "+idk).eq(i);
+        toggleClassForACard(el,urls[i],typeOfGame,"up");
     }
     setTimeout(function () {
         isPause=false;
         for(var i =0;i<urls.length;i++){
-            var el=$("#board div img").eq(i);
-            el.removeClass("flip");
-            el.attr("src","backimg.jpg");
+            var el=$("#board div "+idk).eq(i);
+            toggleClassForACard(el,null,typeOfGame,"down");
 
 
         }
     },1000);
 }
 
-function flipCard(card, imageURL) {
-    if(isPause===true)
+function flipCard(card, url,cardType) {
+    console.log("ENTERED");
+    if(isPause===true || solvedUrls.includes(url))
         return;
     if (lastFlipped == null) {
         lastFlipped = card;
-        card.toggleClass("flip");
-        card.attr("src", imageURL);
+        toggleClassForACard(card,url,cardType,"up");
     } else {
-
         if(lastFlipped.get(0)===card.get(0)) {
-
             return;
         }
-        card.toggleClass("flip");
-        card.attr("src", imageURL);
-        if (card.attr("src") === lastFlipped.attr("src")) {
-            solved.push(lastFlipped.attr("src"));
+        toggleClassForACard(card,url,cardType,"up");
+        if (equalCards(card,lastFlipped,cardType)) {
+
+            solved=Number(solved)+1;
+            solvedUrls.push(url);
             setTimeout(function () {
-                if(solved.length===imagesURLs.length/2)
+                if(solved===imagesURLs.length/2)
+                {
+                    isPause=true;
                     alert("GG WP");
+                }
             },500);
             lastFlipped=null;
         }
@@ -118,11 +109,9 @@ function flipCard(card, imageURL) {
             isPause=true;
             setTimeout(function () {
                 isPause=false;
-                lastFlipped.attr("src", "backimg.jpg");
-                lastFlipped.toggleClass("flip");
+                toggleClassForACard(lastFlipped,null,cardType,"down");
                 lastFlipped = null;
-                card.toggleClass("flip");
-                card.attr("src", "backimg.jpg");
+                toggleClassForACard(card,null,cardType,"down");
             },750);
 
 
@@ -132,3 +121,40 @@ function flipCard(card, imageURL) {
 
 
 }
+
+function equalCards(card1,card2,cardType) {
+    if(cardType==="image"){
+        return card1.attr("src")===card2.attr("src");
+    }
+    else{
+        return card1.text()===card2.text();
+    }
+
+}
+
+function toggleClassForACard(card,value,cardType,status){
+    console.log(card,value,cardType,status);
+    card.toggleClass("flip");
+    if(cardType==="image"){
+        if(status==="down")
+            card.attr("src","backimg.jpg");
+        else
+            card.attr("src",value);
+    }
+    else{
+        if(status==="down")
+            card.text("X");
+        else
+            card.text(value);
+    }
+
+}
+
+
+$(function() {
+    typeOfGame="number";        //set this to change game type
+    if(typeOfGame==="number")
+        flipAll(numbersURLs);
+    else
+        flipAll(imagesURLs);
+});
